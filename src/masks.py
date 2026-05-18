9004080842254e0078d8d7c4961db9caea250aec
Uliana Krasnova <ylianakrasnova90@gmail.com>
2026-05-18 19:03:10 +0300

    create gitignore

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+
+def get_mask_card_number(number: Union[int, str]) -> str:
+    str_number = str(number)
+    if len(str_number) == 16:
+        result = str_number[:6] + "*" * 6 + str_number[-4:]
+        return f"{result[:4]} {result[4:8]} {result[8:12]} {result[12:16]}"
+
+
+def get_mask_account(account: Union[int, str]) -> str:
+    str_account = str(account)
+    if len(str_account) >= 4:
+        return "**" + str_account[-4:]
