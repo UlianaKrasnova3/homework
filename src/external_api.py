a7d7668330ca3941cef9271cea0aa230644a6e5e
Uliana Krasnova <ylianakrasnova90@gmail.com>
2026-09-17 14:03:38 +0300

    homework_12_2 done

--- src/external_api.py
+++ src/external_api.py
@@ -3,6 +3,15 @@ import os
 import requests
 from dotenv import load_dotenv
 
+import logging
+
+logger = logging.getLogger("external_api")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("C:/Users/User/PycharmProjects/homework/logs/external_api.log", encoding='utf-8')
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
@@ -13,25 +22,32 @@ def conversion(transaction: dict) -> float:
     Функция возвращает сумму транзакции и при необходимости конвертирует ее в рубли
     '''
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        logger.info(f'Получаем сумму транзакции в рублях')
         return transaction["operationAmount"]["amount"]
     else:
-        currency = transaction["operationAmount"]["currency"]["code"]
-        amount = transaction["operationAmount"]["amount"]
+        try:
+            logger.info(f'Получаем сумму транзакции через API и переводим в рубли')
+            currency = transaction["operationAmount"]["currency"]["code"]
+            amount = transaction["operationAmount"]["amount"]
 
-        url = "https://api.apilayer.com/exchangerates_data/convert"
+            url = "https://api.apilayer.com/exchangerates_data/convert"
 
-        payload = {
-            "amount": amount,
-            "from": currency,
-            "to": "RUB"
-        }
+            payload = {
+                "amount": amount,
+                "from": currency,
+                "to": "RUB"
+            }
 
-        headers = {
-            "apikey": API_KEY
-        }
+            headers = {
+                "apikey": API_KEY
+            }
 
-        response = requests.get(url, headers=headers, params=payload)
+            response = requests.get(url, headers=headers, params=payload)
 
-        result = response.json()
+            result = response.json()
 
-        return result["result"]
+            return result["result"]
+
+        except Exception as e:
+            logger.error(f'Произошла ошибка {e}')
+            return "Произошла ошибка, проверьте данные"
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,25 @@
 import json
 import os
 
+import logging
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("C:/Users/User/PycharmProjects/homework/logs/utils.log", encoding='utf-8')
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 
 def fin_transaction(json_file_name: str) -> list:
     '''Возвращает данные о финансовых транзакциях'''
     try:
+        logger.info(f'Открываем файл {json_file_name}')
         path = os.path.join('..', 'data', json_file_name)
         with open(path, 'r', encoding='utf-8') as f:
             list_fin_transaction = json.load(f)
         return list_fin_transaction
 
     except Exception as e:
+        logger.error(f'Произошла ошибка {e}')
         return []
