9545a81ecc770303173535f81de6c34fddeeeee5
Uliana Krasnova <ylianakrasnova90@gmail.com>
2026-09-19 01:42:48 +0300

    changed paths to files homework_12_2 done

--- src/masks.py
+++ src/masks.py
@@ -2,12 +2,17 @@ import logging
 import os
 from typing import Union
 
-logs_path = 'logs'
-os.makedirs(logs_path, exist_ok=True)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+logs_dir = os.path.abspath(os.path.join(current_dir, "../logs"))
+os.makedirs(logs_dir, exist_ok=True)
+
+rel_file_path = os.path.join(current_dir, "../logs/masks.log")
+abs_file_path = os.path.abspath(rel_file_path)
 
 logger = logging.getLogger("masks")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("C:/Users/User/PycharmProjects/homework/logs/masks.log", encoding='utf-8')
+file_handler = logging.FileHandler(abs_file_path, encoding='utf-8')
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,17 @@ import json
 import logging
 import os
 
-logs_path = 'logs'
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-os.makedirs(logs_path, exist_ok=True)
+logs_dir = os.path.abspath(os.path.join(current_dir, "../logs"))
+os.makedirs(logs_dir, exist_ok=True)
+
+rel_file_path = os.path.join(current_dir, "../logs/utils.log")
+abs_file_path = os.path.abspath(rel_file_path)
 
 logger = logging.getLogger("utils")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("C:/Users/User/PycharmProjects/homework/logs/utils.log", encoding='utf-8')
+file_handler = logging.FileHandler(abs_file_path, encoding='utf-8')
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
